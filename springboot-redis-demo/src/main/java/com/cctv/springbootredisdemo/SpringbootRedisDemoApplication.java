8dd7ea4fe1a845adc58be71f3b3ed79a8d5af0dc
ericzya <ericzya@outlook.com>
2020-03-05 11:37:16 +0800

    --2020.3.5 1.fix some bugs

---
+++ springboot-redis-demo/src/main/java/com/cctv/springbootredisdemo/SpringbootRedisDemoApplication.java
@@ -0,0 +1,16 @@
+package com.cctv.springbootredisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ericz
+ */
+@SpringBootApplication
+public class SpringbootRedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRedisDemoApplication.class, args);
+    }
+
+}
---
+++ springboot-redis-demo/src/test/java/com/cctv/springbootredisdemo/SpringbootRedisDemoApplicationTests.java
@@ -0,0 +1,27 @@
+package com.cctv.springbootredisdemo;
+
+import com.cctv.springbootredisdemo.service.manager.svc.ManagerService;
+import com.cctv.springbootredisdemo.service.user.svc.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisDemoApplicationTests {
+
+    @Autowired
+    private ManagerService managerService;
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void managerDataSource() {
+        System.out.println("测试");
+        System.out.println("错误管理员：" + (managerService.loginCheck("ericzya@outlook.com", "1") ? "登陆成功" : "登陆失败"));
+        System.out.println("正确管理员：" + (managerService.loginCheck("ericzya@outlook.com", "112233") ? "登陆成功" : "登陆失败"));
+        System.out.println("错误用户：" + (userService.loginCheck("ericzya@outlook.com", "1") ? "登陆成功" : "登陆失败"));
+        System.out.println("正确用户：" + (userService.loginCheck("ericzya@outlook.com", "445566") ? "登陆成功" : "登陆失败"));
+    }
+
+}
