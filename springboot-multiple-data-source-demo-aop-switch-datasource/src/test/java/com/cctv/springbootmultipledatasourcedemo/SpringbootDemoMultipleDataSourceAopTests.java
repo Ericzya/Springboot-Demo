bbaaaf3b188bce98d68f0d26d160aaa5cf4f19be
ericzya <ericzya@outlook.com>
2020-03-13 09:40:50 +0800

    --2020.3.13 1.add some demos

--- springboot-multiple-data-source-demo-aop-switch-datasource/src/test/java/com/cctv/springbootmultipledatasourcedemo/SpringbootDemoMultipleDataSourceAopTests.java
+++ springboot-multiple-data-source-demo-aop-switch-datasource/src/test/java/com/cctv/springbootmultipledatasourcedemo/SpringbootDemoMultipleDataSourceAopTests.java
@@ -1,5 +1,9 @@
 package com.cctv.springbootmultipledatasourcedemo;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.cctv.springbootmultipledatasourcedemo.persistent.manager.pl.ManagerDAO;
+import com.cctv.springbootmultipledatasourcedemo.persistent.manager.po.Manager;
 import com.cctv.springbootmultipledatasourcedemo.service.manager.svc.ManagerService;
 import com.cctv.springbootmultipledatasourcedemo.service.user.svc.UserService;
 import org.junit.jupiter.api.Test;
@@ -17,6 +21,9 @@ class SpringbootDemoMultipleDataSourceAopTests {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ManagerDAO managerDAO;
+
     @Test
     void managerDataSource() {
         System.out.println("测试");
@@ -24,5 +31,17 @@ class SpringbootDemoMultipleDataSourceAopTests {
         System.out.println("正确管理员：" + (managerService.loginCheck("ericzya@outlook.com", "112233") ? "登陆成功" : "登陆失败"));
         System.out.println("错误用户：" + (userService.loginCheck("ericzya@outlook.com", "1") ? "登陆成功" : "登陆失败"));
         System.out.println("正确用户：" + (userService.loginCheck("ericzya@outlook.com", "445566") ? "登陆成功" : "登陆失败"));
+    }
+
+    @Test
+    void JsonTest() {
+        Manager manager = managerDAO.getManagerByEmail("ericzya@outlook.com");
+        String s = JSON.toJSONString(manager);
+        System.out.println(s);
+
+        JSONObject jsonObject = JSONObject.parseObject(s);
+        String accountPassword = jsonObject.getString("accountPassword");
+        String emailAddress = jsonObject.getString("emailAddress");
+        System.out.println(accountPassword + "--" + emailAddress);
     }
 }
