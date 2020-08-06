4930c21039a3663d74731e33fe772737538f817e
Eric <ericzya@outlook.com>
2020-08-06 18:07:51 +0800

    feat:reconstruct project

---
+++ springboot-model/src/main/java/com/cctv/springbootdemo/model/vo/VisitorVO.java
@@ -0,0 +1,36 @@
+package com.cctv.springbootdemo.model.vo;
+
+/**
+ * @Author: Eric.Zhang
+ * @Description: 访问者VO
+ * @ProjectName: springboot-demo
+ * @Date: 2020/8/6 17:56
+ */
+public class VisitorVO {
+    private String inputAccount;
+    private String inputPassword;
+
+    public VisitorVO() {
+    }
+
+    public VisitorVO(String inputAccount, String inputPassword) {
+        this.inputAccount = inputAccount;
+        this.inputPassword = inputPassword;
+    }
+
+    public String getInputAccount() {
+        return inputAccount;
+    }
+
+    public void setInputAccount(String inputAccount) {
+        this.inputAccount = inputAccount;
+    }
+
+    public String getInputPassword() {
+        return inputPassword;
+    }
+
+    public void setInputPassword(String inputPassword) {
+        this.inputPassword = inputPassword;
+    }
+}
