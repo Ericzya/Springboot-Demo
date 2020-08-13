899a15a70879ea56a6b5823c910b2a0a6208baae
Eric <ericzya@outlook.com>
2020-08-13 09:11:25 +0800

    feat:当用户加载不正确时记录log

--- springboot-demo-spring-security/src/main/java/com/cctv/springbootdemo/config/security/userdetailservice/McoolUserDetailsServiceImpl.java
+++ springboot-demo-spring-security/src/main/java/com/cctv/springbootdemo/config/security/userdetailservice/McoolUserDetailsServiceImpl.java
@@ -2,6 +2,8 @@ package com.cctv.springbootdemo.config.security.userdetailservice;
 
 import com.cctv.springbootdemo.service.visitor.bo.VisitorBO;
 import com.cctv.springbootdemo.service.visitor.svc.VisitorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,7 +17,7 @@ import org.springframework.util.StringUtils;
  */
 @Service
 public class McoolUserDetailsServiceImpl implements UserDetailsService {
-
+    private final static Logger logger = LoggerFactory.getLogger(McoolUserDetailsServiceImpl.class);
     private final VisitorService visitorService;
 
     @Autowired
@@ -26,10 +28,12 @@ public class McoolUserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         if (StringUtils.isEmpty(s)) {
+            logger.error("用户名不能为空！");
             throw new UsernameNotFoundException("用户名不能为空！");
         }
         VisitorBO visitorBO = visitorService.getVisitorBO(s);
         if (visitorBO == null) {
+            logger.error("用户名不存在！");
             throw new UsernameNotFoundException("用户名不存在！");
         }
         return visitorBO;
