a288a4835f334fbcae52ccc37a1b461f2670362f
Eric <ericzya@outlook.com>
2020-08-19 11:27:01 +0800

    feat:Fix comments

--- springboot-demo-spring-security/src/main/java/com/cctv/springbootdemo/config/security/passwordencoder/McoolPasswordEncoder.java
+++ springboot-demo-spring-security/src/main/java/com/cctv/springbootdemo/config/security/passwordencoder/McoolPasswordEncoder.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * @Author: Eric.Zhang
- * @Description: 密码加密编码器（为使用wireshark测试http连接此处暂不加密）
+ * @Description: 密码加密编码器
  * @ProjectName: springboot-demo-spring-security
  * @Date: 2020/7/30 17:25
  */
