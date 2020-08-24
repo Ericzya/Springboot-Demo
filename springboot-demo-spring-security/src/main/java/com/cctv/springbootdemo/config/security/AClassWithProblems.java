1d723d2691710903e6eeaf83c46393ffb929e1fc
Eric <ericzya@outlook.com>
2020-08-24 17:55:13 +0800

    feat:Add a java class with syntax errors.

---
+++ springboot-demo-spring-security/src/main/java/com/cctv/springbootdemo/config/security/AClassWithProblems.java
@@ -0,0 +1,20 @@
+package com.cctv.springbootdemo.config.security;
+
+
+import java.util.List;
+
+public class AClassWithProblems {
+    static String aaa;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 11111; i++) {
+            System.out.println(aaa);
+        }
+    }
+
+    private void aa_buzhidaoshenmefangfa() {
+        List a = null;
+        System.out.println(a.toString());
+    }
+
+}
