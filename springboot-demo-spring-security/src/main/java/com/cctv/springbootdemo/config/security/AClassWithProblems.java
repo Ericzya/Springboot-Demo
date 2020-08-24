bc55e1a9c7bdf98e9b21b64c526eddfb5c25a7b8
Eric <ericzya@outlook.com>
2020-08-24 18:05:47 +0800

    feat:Add a java class with syntax errors.

--- springboot-demo-spring-security/src/main/java/com/cctv/springbootdemo/config/security/AClassWithProblems.java
+++ springboot-demo-spring-security/src/main/java/com/cctv/springbootdemo/config/security/AClassWithProblems.java
@@ -4,7 +4,7 @@ package com.cctv.springbootdemo.config.security;
 import java.util.List;
 
 public class AClassWithProblems {
-    static String aaa;
+    final static String aaa;
 
     public static void main(String[] args) {
         for (int i = 0; i < 11111; i++) {
