0b1ab50ef286ab4d7d95fbdedfd2bbc7e6c5e0e6
Eric <ericzya@outlook.com>
2020-08-25 09:40:01 +0800

    feat:Update some contents.

--- springboot-demo-spring-security/src/main/java/com/cctv/springbootdemo/config/security/processhandler/SecurityAuthenticationSuccessHandler.java
+++ springboot-demo-spring-security/src/main/java/com/cctv/springbootdemo/config/security/processhandler/SecurityAuthenticationSuccessHandler.java
@@ -4,6 +4,7 @@ import com.cctv.springbootdemo.service.visitor.bo.VisitorBO;
 import com.cctv.springbootdemo.service.visitor.svc.VisitorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -33,6 +34,7 @@ public class SecurityAuthenticationSuccessHandler extends SavedRequestAwareAuthe
     private final VisitorService visitorService;
     private final RequestCache requestCache = new HttpSessionRequestCache();
 
+    @Autowired
     public SecurityAuthenticationSuccessHandler(VisitorService visitorService) {
         this.visitorService = visitorService;
     }
